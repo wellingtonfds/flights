e0f92cd2af2a30dfe5519bc1c9691acbdc2186d9
Wellington Fernandes <wellingtonfds@gmail.com>
2021-07-14 14:40:02 +0000

    Initial commit from PHPSandbox

---
+++ routes/api.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Collection;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/test', function (Request $request) {
+    $response = Http::get('http://prova.123milhas.net/api/flights');
+    $flights = new Collection($response->json());
+    $groupFare = $flights->groupBy(['fare','outbound','price']);
+    $groupFlights = $groupFare->map(function($outbounds){
+    $newGroup = [];
+        
+        foreach($outbounds as $key=>$outbound){
+            foreach($outbound as $price){
+                if($key){
+                $newGroup['outbound'][] = $price;
+                continue;
+                }
+                $newGroup['inbound'][] = $price;
+               
+            }
+        }
+        $groups = [];
+        foreach($newGroup['inbound'] as $inbound){
+            foreach($newGroup['outbound'] as $outbound){
+                $groups[] = [
+                    'inbound'=>$inbound,
+                    'outbound'=>$outbound
+                ];
+            }
+        }
+        return $groups;
+    });
+    dd($groupFlights);
+
+
+
+    // $groupFlights->;
+
+    
+});
