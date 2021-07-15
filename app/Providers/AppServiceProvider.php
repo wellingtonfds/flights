364e08693bf753483556aab1026e7b2cd37e5d96
wellington fernandes <wellingtonfds@gmail.com>
2021-07-15 09:39:02 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Services\flights\FlightsServices;
 use App\Services\flights\FlightsServicesInterface;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
     }
 }
