4a5cb823d8adc8b6c14bead637058bfc3450029d
wellington fernandes <wellingtonfds@gmail.com>
2021-07-15 09:42:08 -0300

    check https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
+        if (app()->environment('remote')) {
             URL::forceSchema('https');
         }
     }
